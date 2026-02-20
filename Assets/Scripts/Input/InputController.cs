a26c1517397c0a6e4b74dd03a21121ea7d867311
zhek0s <79040843+zhek0s@users.noreply.github.com>
2026-02-20 13:10:16 +0200

    add horizontal drag

--- Assets/Scripts/Input/InputController.cs
+++ Assets/Scripts/Input/InputController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InputController : MonoBehaviour
 {
     [SerializeField] private float launchForce = 10f;
+    [SerializeField] private float xLimit = 2f;
     private Cube currentCube;
 
     public void SetCube(Cube cube)
@@ -19,11 +20,23 @@ public class InputController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0) && currentCube != null)
+        if (currentCube == null) return;
+
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.mousePosition.x;
+            float t = Mathf.InverseLerp(0,Screen.width, mouseX);
+            float needX = Mathf.Lerp(-xLimit, xLimit, t);
+            Vector3 pos = currentCube.transform.position;
+            float changeX = needX - pos.x;
+            pos.x = Mathf.Clamp(pos.x+ changeX, -xLimit, xLimit);
+            currentCube.transform.position = pos;
+        }
+
+        if (Input.GetMouseButtonUp(0))
         {
             currentCube.Launch(launchForce);
             currentCube = null;
         }
-
     }
 }
