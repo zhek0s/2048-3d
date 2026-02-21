ed10aa606e037c296f3bc56c2e7bd7154a0280ac
zhek0s <79040843+zhek0s@users.noreply.github.com>
2026-02-21 20:24:57 +0200

    add `MergeAnimation` script

---
+++ Assets/Scripts/Animations/MergeAnimation.cs
@@ -0,0 +1,53 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeAnimation : MonoBehaviour
+{
+    public static async UniTask MoveCubeAsync(Cube cube, Vector3 needPos, float startSpeed)
+    {
+        float speed = startSpeed;
+        float maxSpeed = 10;
+        float accelaration = 20;
+        float remainDistance = 1;
+        while (remainDistance > 0.5f)
+        {
+            await UniTask.NextFrame();
+            speed = MoveCube(cube, needPos, maxSpeed, accelaration, speed, out remainDistance);
+        }
+    }
+
+    public static async UniTask Move2CubesTowardsAsync(Cube cube, Cube otherCube, float startSpeed)
+    {
+        float speed = startSpeed;
+        float maxSpeed = 10;
+        float accelaration = 20;
+        float remainDistance = 1;
+        while (remainDistance > 0.5f)
+        {
+            await UniTask.NextFrame();
+            speed = MoveCube(cube, otherCube.transform.position, maxSpeed, accelaration, speed, out remainDistance);
+        }
+    }
+
+    private static float MoveCube(
+        Cube cube,
+        Vector3 needPos,
+        float maxSpeed,
+        float accelaration,
+        float speed,
+        out float remainDistance
+    )
+    {
+        Vector3 direction = needPos - cube.transform.position;
+        remainDistance = direction.magnitude;
+        direction = direction / remainDistance;
+        speed = Mathf.Min(speed + accelaration * Time.deltaTime, maxSpeed);
+        float tickDistance = speed * Time.deltaTime;
+        tickDistance = Mathf.Min(remainDistance, tickDistance);
+        cube.transform.position += tickDistance * direction;
+        remainDistance -= tickDistance;
+        return speed;
+    }
+}
